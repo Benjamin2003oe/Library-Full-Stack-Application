87698302d4cb378e3567916cd6a806749261c417
JEZ37S <kvbenjamin@stud.uni-obuda.hu>
2024-03-17 19:34:30 +0100

    made some changes in js and in css

--- JEZ37S_HFT_2023241.JSClient/wwwroot/script.js
+++ JEZ37S_HFT_2023241.JSClient/wwwroot/script.js
@@ -1,3 +1,18 @@
-﻿fetch('http://localhost:13009/book')
+﻿let books = [];
+
+fetch('http://localhost:13009/book')
     .then(x => x.json())
-    .then(y => console.log(y));
+    .then(y => {
+        books = y;
+        console.log(books);
+        display();
+    });
+
+
+function display() {
+    books.forEach(t => {
+        document.getElementById('resultarea').innerHTML +=
+            "<tr><td>" + t.id + "</td><td>"
+            + t.name + "</td></tr>";
+    });
+}
