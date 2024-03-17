15a73647429999862f31d09fa65e55ae2ea38e4c
JEZ37S <kvbenjamin@stud.uni-obuda.hu>
2024-03-17 22:12:20 +0100

    POST and DELETE functions are done

--- JEZ37S_HFT_2023241.JSClient/wwwroot/script.js
+++ JEZ37S_HFT_2023241.JSClient/wwwroot/script.js
@@ -1,18 +1,53 @@
 ﻿let books = [];
 
-fetch('http://localhost:13009/book')
-    .then(x => x.json())
-    .then(y => {
-        books = y;
-        console.log(books);
-        display();
-    });
+getData();
 
+async function getData() {
+    await fetch('http://localhost:13009/book')
+        .then(x => x.json())
+        .then(y => {
+            books = y;
+            console.log(books);
+            display();
+        });
+}
 
 function display() {
+    document.getElementById('resultarea').innerHTML = "";
     books.forEach(t => {
         document.getElementById('resultarea').innerHTML +=
             "<tr><td>" + t.id + "</td><td>"
-            + t.name + "</td></tr>";
+        + t.name + "</td><td>" +
+            `<button type="button" onclick="remove(${t.id})">Delete</button>`
+            + "</td></tr>";
     });
+}
+
+function remove(id){
+    fetch('http://localhost:13009/book/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getData();
+        })
+        .catch((error) => { console.error('Error:', error); });
+}
+
+function create() {
+    let gottenName = document.getElementById('bookname').value;
+    fetch('http://localhost:13009/book', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json',},
+        body: JSON.stringify(
+            { name: gottenName }),})
+        .then(response => response)
+        .then(data =>
+        {
+            console.log('Success:', data);
+            getData();
+        })
+        .catch((error) => { console.error('Error:', error); });
 }
