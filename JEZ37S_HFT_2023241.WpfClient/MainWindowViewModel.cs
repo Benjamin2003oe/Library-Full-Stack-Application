1fbd07b44b8e94834bd28a6527b73f7adcc06677
JEZ37S <kvbenjamin@stud.uni-obuda.hu>
2024-03-17 13:54:26 +0100

    WpfClient and MainWindowViewModel has been created

---
+++ JEZ37S_HFT_2023241.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using JEZ37S_HFT_2023241.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JEZ37S_HFT_2023241.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Book> Books { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Books = new RestCollection<Book>("http://localhost:13009/", "book");
+        }
+    }
+}
