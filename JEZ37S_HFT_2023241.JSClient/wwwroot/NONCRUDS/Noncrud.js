1234739cf3acff10d42c1cce5883437308820347
JEZ37S <kvbenjamin@stud.uni-obuda.hu>
2024-04-04 17:14:24 +0200

    JSClient NONCRUDS done/working

--- JEZ37S_HFT_2023241.JSClient/wwwroot/NONCRUDS/Noncrud.js
+++ JEZ37S_HFT_2023241.JSClient/wwwroot/NONCRUDS/Noncrud.js
@@ -4,6 +4,7 @@ function cleardisplay() {
     document.getElementById('noncrudform').style.display = "none";
     document.getElementById('form_BooksWrittenbyAuthor').style.display = "none";
     document.getElementById('form_AuthorsBornYear').style.display = "none";
+    document.getElementById('form_WhoReservedThisBook').style.display = "none";
     document.getElementById('form_HowManyBooksPerCategory').style.display = "none";
 }
 
@@ -11,16 +12,15 @@ async function getdata03() {
     cleardisplay();
     document.getElementById('form_BooksWrittenbyAuthor').style.display = "initial";
     document.getElementById('result_BooksWrittenbyAuthor').innerHTML = "";
-    await getBooks();
-    Books.forEach(function (item) {
+    await getAuthors();
+    authors.forEach(function (item) {
         let o = document.createElement("option");
-        o.text = item.Book_name;
-        o.value = item.Book_name;
+        o.text = item.name;
+        o.value = item.name;
         document.getElementById('slctAuthor').appendChild(o);
         console.log(o);
     })
 }
-
 
 let Books = [];
 async function getBooks() {
@@ -32,23 +32,41 @@ async function getBooks() {
         });
 }
 
+let Categories = [];
+async function getCategories() {
+    await fetch('http://localhost:13009/category')
+        .then(x => x.json())
+        .then(y => {
+            Categories = y;
+            console.log(Categories);
+        });
+}
+
+let authors = [];
+async function getAuthors() {
+    await fetch('http://localhost:13009/author')
+        .then(x => x.json())
+        .then(y => {
+            authors = y;
+            console.log(authors);
+        });
+}
+
 function display03() {
     cleardisplay();
     document.getElementById('form_BooksWrittenbyAuthor').style.display = "initial";
     document.getElementById('result_BooksWrittenbyAuthor').innerHTML = "";
     noncrudcoll.forEach(t => {
-        console.log(t.Book_name);
+        console.log(t.name);
         document.getElementById('result_BooksWrittenbyAuthor').innerHTML +=
-            "<tr><td>" + t.Book_name + "</td></tr>";
+            "<tr><td>" + t.name + "</td></tr>";
         console.log(t);
     })
 }
 
-
 async function getdata03_final() {
     let str = document.getElementById('slctAuthor').value;
-    str = "http://localhost:13009/Stat/BooksWrittenbyAuthor/" + str;
-
+    str = "http://localhost:13009/Stat/GetAuthorBooks/" + str;
     await fetch(str)
         .then(x => x.json())
         .then(y => {
@@ -58,7 +76,6 @@ async function getdata03_final() {
         });
 }
 
-
 async function getdata04() {
     cleardisplay();
     document.getElementById('form_AuthorsBornYear').style.display = "initial";
@@ -66,8 +83,8 @@ async function getdata04() {
     await getBooks();
     Books.forEach(function (item) {
         let o = document.createElement("option");
-        o.text = item.Book_name;
-        o.value = item.Book_name;
+        o.text = item.name;
+        o.value = item.name;
         document.getElementById('slctBook1').appendChild(o);
         console.log(o);
     })
@@ -78,16 +95,16 @@ function display04() {
     document.getElementById('form_AuthorsBornYear').style.display = "initial";
     document.getElementById('result_AuthorsBornYear').innerHTML = "";
     noncrudcoll.forEach(t => {
-        console.log(t.teachername);
+        console.log(t.year);
         document.getElementById('result_AuthorsBornYear').innerHTML +=
-            "<tr><td>" + t.teachername + "</td></tr>";
+            "<tr><td>" + t.year + "</td></tr>";
         console.log(t);
     })
 }
 
 async function getdata04_final() {
     let str = document.getElementById('slctBook1').value;
-    str = "http://localhost:13009/Stat/AuthorsBornYear/" + str;
+    str = "http://localhost:13009/Stat/WhenWasTheAuthorBorn/" + str;
 
     await fetch(str)
         .then(x => x.json())
@@ -98,8 +115,6 @@ async function getdata04_final() {
         });
 }
 
-
-
 async function getdata05() {
     cleardisplay();
     document.getElementById('form_WhoReservedThisBook').style.display = "initial";
@@ -107,8 +122,8 @@ async function getdata05() {
     await getBooks();
     Books.forEach(function (item) {
         let o = document.createElement("option");
-        o.text = item.Book_name;
-        o.value = item.Book_name;
+        o.text = item.name;
+        o.value = item.name;
         document.getElementById('slctBook2').appendChild(o);
         console.log(o);
     })
@@ -119,16 +134,16 @@ function display05() {
     document.getElementById('form_WhoReservedThisBook').style.display = "initial";
     document.getElementById('result_WhoReservedThisBook').innerHTML = "";
     noncrudcoll.forEach(t => {
-        console.log(t.studentname);
+        console.log(t.membername);
         document.getElementById('result_WhoReservedThisBook').innerHTML +=
-            "<tr><td>" + t.studentname + "</td></tr>";
+            "<tr><td>" + t.membername + "</td></tr>";
         console.log(t);
     })
 }
 
 async function getdata05_final() {
     let str = document.getElementById('slctBook2').value;
-    str = "http://localhost:13009/Stat/WhoReservedThisBook/" + str;
+    str = "http://localhost:13009/Stat/Reservedby/" + str;
 
     await fetch(str)
         .then(x => x.json())
@@ -139,30 +154,30 @@ async function getdata05_final() {
         });
 }
 
-
-
 async function getdata06() {
     cleardisplay();
     document.getElementById('form_HowManyBooksPerCategory').style.display = "initial";
     document.getElementById('result_HowManyBooksPerCategory').innerHTML = "";
-    await getSubjects();
-    subjects.forEach(function (item) {
+    await getCategories();
+    Categories.forEach(function (item) {
         let o = document.createElement("option");
-        o.text = item.subject_Name;
-        o.value = item.subject_Name;
+        o.text = item.category_Name;
+        o.value = item.category_Name;
         document.getElementById('slctCategory').appendChild(o);
         console.log(o);
     })
 }
 
+async function getdata06_final() {
+    let str = document.getElementById('slctCategory').value;
+    str = "http://localhost:13009/Stat/CountBooksPerCategory/" + str;
 
-let subjects = [];
-async function getSubjects() {
-    await fetch('http://localhost:13009/Subject')
+    await fetch(str)
         .then(x => x.json())
         .then(y => {
-            subjects = y;
-            console.log(subjects);
+            noncrudcoll = y;
+            console.log(noncrudcoll)
+            display06();
         });
 }
 
@@ -172,6 +187,6 @@ function display06() {
     document.getElementById('result_HowManyBooksPerCategory').innerHTML = "";
     noncrudcoll.forEach(t => {
         document.getElementById('result_HowManyBooksPerCategory').innerHTML +=
-            "<tr><td>" + t.teachername + "</td></tr>";
+            "<tr><td>" + t.name + "</td></tr>";
     })
 }
